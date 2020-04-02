e06f513950ebb252a0938468119182dedc4e65b5
Mirana1 <legendsofgueneviere@gmail.com>
2020-04-02 19:58:11 +0300

    Migration, added customCss file.

---
+++ Data/ITSkills.Data/Migrations/20200402134736_AddedRequirementsAcquiredKnowledgeToCourseEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ITSkills.Data.Migrations
+{
+    public partial class AddedRequirementsAcquiredKnowledgeToCourseEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AcquiredKnowledge",
+                table: "Courses",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Requirements",
+                table: "Courses",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcquiredKnowledge",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Requirements",
+                table: "Courses");
+        }
+    }
+}
---
+++ Data/ITSkills.Data/Migrations/20200402164852_AddedAboutInfoInUser.cs
@@ -0,0 +1,22 @@
+﻿namespace ITSkills.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddedAboutInfoInUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AboutInfo",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AboutInfo",
+                table: "AspNetUsers");
+        }
+    }
+}
