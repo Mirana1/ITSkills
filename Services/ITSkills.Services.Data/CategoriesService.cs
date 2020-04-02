07b954d1c66f71d45f96e6076ccbb6c693e8a00e
Mirana1 <legendsofgueneviere@gmail.com>
2020-04-02 15:10:09 +0300

    Added favicon, partial views.

--- Services/ITSkills.Services.Data/CategoriesService.cs
+++ Services/ITSkills.Services.Data/CategoriesService.cs
@@ -1,8 +1,6 @@
 ﻿namespace ITSkills.Services.Data
 {
-    using System;
     using System.Collections.Generic;
-    using System.Globalization;
     using System.Linq;
 
     using ITSkills.Data.Common.Repositories;
