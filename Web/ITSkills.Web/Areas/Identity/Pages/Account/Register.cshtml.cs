72850cc264806b95e1ef67e561f54c960e0a9760
Mirana1 <legendsofgueneviere@gmail.com>
2020-04-08 18:19:12 +0300

    Changed username to be equal to username, not email.

--- Web/ITSkills.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web/ITSkills.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,7 +80,7 @@
             this.ExternalLogins = (await this.signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (this.ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = this.Input.Email, Email = this.Input.Email };
+                var user = new ApplicationUser { UserName = this.Input.Username, Email = this.Input.Email };
                 var result = await this.userManager.CreateAsync(user, this.Input.Password);
                 if (result.Succeeded)
                 {
