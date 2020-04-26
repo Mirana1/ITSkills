509c79ee81851fcc0c13bb8fb4ff78e9f4cfd65b
Mirana1 <46326466+Mirana1@users.noreply.github.com>
2020-04-26 14:53:35 +0300

    Update GlobalConstants.cs

--- ITSkills.Common/GlobalConstants.cs
+++ ITSkills.Common/GlobalConstants.cs
@@ -5,12 +5,7 @@
         public const string SystemName = "ITSkills";
 
         public const string AdministratorRoleName = "Administrator";
-
-<<<<<<< HEAD
-=======
-        public const string SendGridAPIKey = "";
-
->>>>>>> 7c168214842b8a626c78b4f60e1ad1574cd6901d
+        
         public const string CategoryTitle = "Category";
     }
 }
